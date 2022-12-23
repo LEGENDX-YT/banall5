fb67b3e9f0c372ce297705c1f82a735464e2b8c7
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-12-23 09:04:58 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# banall5
+# Ban All Bot
+
+- This is a Sudo Based Members Banning Bot 
+ 
+# Commands
+- ping
+- banall
+- unbanall
+- kickall
+- leave 
+- restart
+
+# Deploy To Heroku 🚀
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/MrRizoel/BanAllBot)
+
+# Credits
+* [RiZoeL](https://github.com/MrRizoel)
+* [Lonami](https://github.com/LonamiWebs/) for [Telethon.](https://github.com/LonamiWebs/Telethon)
---
+++ var.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+
+def make_int(str_input):
+    str_list = str_input.split(" ")
+    int_list = []
+    for x in str_list:
+        int_list.append(int(x))
+    return int_list
+
+class Var:
+    API_ID = int(os.getenv("API_ID", "29495557"))
+    API_HASH = os.getenv("API_HASH", "e5efba8eba82bd702ce0740c2cfa6281")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5358244559:AAHKMJmu2wpfbuE4KPoHVvDB2xZB_nGtJzc")
+    sudo = os.getenv("SUDO", "5526165514 5778117994")
+    SUDO = []
+    if sudo:
+        SUDO = make_int(sudo)
